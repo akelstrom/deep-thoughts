23ad47baff717928d1d2be492e967355d8ec5bec
akelstrom <akelstrom@gmail.com>
2021-03-31 16:46:28 -0400

    added text area forms with character count on home and profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Redirect, useParams } from "react-router-dom";
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
-import { useQuery } from "@apollo/react-hooks";
+import ThoughtForm from '../components/ThoughtForm';
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import { ADD_FRIEND } from "../utils/mutations";
 import Auth from "../utils/auth";
 
 const Profile = () => {
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -27,18 +30,31 @@ const Profile = () => {
   if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+        You need to be logged in to see this page. Use the navigation links
+        above to sign up or log in!
       </h4>
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div className="flex-row justify-space-between mb-3">
+       <h2 className="bg-dark text-secondary p-3 display-inline-block">
+          Viewing {userParam ? `${user.username}'s` : "your"} profile.
+        </h2>
+       <button className="btn ml-auto" onClick={handleClick}>
+          Add Friend
+        </button>
       <div className="col-12 mb-3 col-lg-8">
-      <h2 className="bg-dark text-secondary p-3 display-inline-block">
-        Viewing {userParam ? `${user.username}'s` : "your"} profile.
-      </h2>
-      
         <ThoughtList
           thoughts={user.thoughts}
           title={`${user.username}'s thoughts...`}
@@ -51,6 +67,7 @@ const Profile = () => {
           friends={user.friends}
         />
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
